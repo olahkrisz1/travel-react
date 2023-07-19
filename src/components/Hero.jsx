98f0afc9904f5a89eba5edb0f15ed96ff1f48811
olahkrisz1 <olahkrisz1@gmail.com>
2023-07-19 17:54:01 +0200

    destinations component added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-4'>
         <h1>First Class Travel</h1>
         <h2 className='py-4'>Top Destinations Worldwide</h2>
-        <form className='flex justify-between items-center max-w[700px] mx-auto w-full border p-1
+        <form className='flex justify-between items-center max-w[700px] mx-auto  border p-1
         rounded-md text-black bg-gray-100/90'>
             <div>
                 <input className='bg-transparent w-[300px] sm:w-[400px] font-[Poppins] focus:outline-none' type='text' placeholder='Search Destinations' />
